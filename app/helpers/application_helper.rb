b1512b47860fa8ed477b8e0e9d3378f6587b4e18
maniva <ivano.mangiarotti@gmail.com>
2011-10-25 16:06:35 +0200

    Added title OK

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 module ApplicationHelper
   def title(*parts)
-    unless parts.empty?
-      content_for :title do
-        (parts << "Ticketee").join(" - ") unless parts.empty?
-      end
-    end
+    @title = (parts << "Ticketee").join(" - ") unless parts.empty?
+    @title || "Ticketee"
   end
 end
