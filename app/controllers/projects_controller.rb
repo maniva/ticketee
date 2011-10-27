11157c9b7becfcf5813dc028fdf95e51980daf3b
maniva <ivano.mangiarotti@gmail.com>
2011-10-27 13:45:54 +0200

    Added updating functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
   if @project.save
     flash[:notice] = "Project has been created."
     redirect_to @project
-  else
+   else
     flash[:alert] = "Project has not been created."
     render :action => "new"
   end
@@ -22,4 +22,19 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
   
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+     else
+      flash[:alert] = "Project has not been updated."
+      render :action => "edit"
+    end
+  end
+
 end
